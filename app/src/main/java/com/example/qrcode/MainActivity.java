dd41564382259d61c8fe0208f96389f39262646d
S00235076 <S00235076@atu.ie>
2024-11-21 09:43:16 +0000

    first commit

---
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.qrcode;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //View Objects
+    private Button buttonScan;
+    private TextView textViewName, textViewAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //View objects
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewAddress = (TextView) findViewById(R.id.textViewAddress);
+    }
+}
